e10181b1130d9efcc41d8d3f362c72291d3053ea
Fgvs-PC <fgvizcainos@gmail.com>
2023-05-10 21:01:54 -0600

    PLantilla 50porciento

---
+++ src/principal/SQLDatabaseConnection.java
@@ -0,0 +1,24 @@
+package principal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SQLDatabaseConnection {
+    // Connect to your database.
+    // Replace server name, username, and password with your credentials
+    public static void main(String[] args) {
+        String connectionUrl =
+                "jdbc:sqlserver://localhost:1433;"
+                        + "database=Tienda;"
+                        + "user=sa;"
+                        + "password=admin;";
+
+        try (Connection connection = DriverManager.getConnection(connectionUrl);) {
+            // Code here.
+        }
+        // Handle any errors that may have occurred.
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
